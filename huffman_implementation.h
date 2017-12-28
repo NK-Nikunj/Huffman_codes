44545baa9087dd1020105fd4489c3257da85863b
Nikunj <nikunj.nkgpta@gmail.com>
2017-12-28 18:25:55 +0530

    added all functionality

--- huffman_implementation.h
+++ huffman_implementation.h
@@ -13,6 +13,8 @@
 
 		string text;
 		int count[256];
+		BT* root;
+		unordered_map<string, string> codes;
 
 	public:
 
@@ -22,6 +24,8 @@
 
 			for(int i = 0 ; i < 256 ; i++) 
 				this->count[i] = count[i];
+
+			root = NULL;
 
 			create_heap();		
 		}
@@ -66,9 +70,16 @@
 				top = new BT(right, left);
 				min_heap.push(top);
 			}
+			
+			root = top;
 
+			generate_codes(top);
+			
 			//print(top, 0);
 			//cout << endl;
+
+			hashmap(top);
+			compress();
 		}
 
 /*
@@ -77,19 +88,105 @@
 			if(top == NULL) 
 				return;
 
-			space += 10;
+			space += 20;
 
 			print(top->right, space);
 
 			cout << endl;
 			for(int i = 10 ; i < space; i++)
 				cout << " ";
-			cout << top->ch << ", " << top->count;
+			cout << top->ch << ", " << top->count << ", " << top->code;
 
 			print(top->left, space); 
 
 		}
 */
+		void generate_codes(BT* top) {
+
+			if(top->left == NULL && top->right == NULL)
+				return;
+
+			top->left->code.append(top->code + "1");
+			top->right->code.append(top->code + "0");
+
+			generate_codes(top->left);
+			generate_codes(top->right);
+		}
+
+		void hashmap(BT* top) {
+
+			if(top->left == NULL && top->right == NULL) {
+				codes[top->ch] = top->code;
+				return;
+			}
+
+			hashmap(top->left);
+			hashmap(top->right);
+		}
+
+		void compress() {
+
+			string size_ = "";
+			cout << "\n\nHuffman code should look like: \n" << endl;
+
+			for(int i = 0 ; i < text.size() ; i++) {
+
+				char ch[1] = {text.at(i)};
+				string to_compress = "";
+				to_compress.append(ch);
+
+				size_ += codes[to_compress];
+
+				cout << codes[to_compress];
+			}
+
+			cout << endl << endl;
+
+			extras(size_);
+		}
+
+		void extras(string size_) {
+
+			char ch;
+			cout << "\nDo you wish to know more about compression? (y/n): ";
+			cin >> ch;
+			
+			while(ch != 'n' && ch != 'y' && ch != 'N' && ch != 'Y') {
+			
+				cout << "Incorrect response, try again!" << endl;
+				cout << "Do you wish to know more about compression? (y/n): ";
+				cin >> ch;
+			
+			}
+			
+			if(ch == 'Y' || ch == 'y')
+				more(size_);
+			else
+				cout << "\nThanks for using!" << endl;
+
+		}
+
+		void more(string size_) {
+
+			cout << "\nHere is the computer genereated code for all the characters: " << endl;
+			print_codes(root);
+
+			cout << "\nThe original text will take " << text.size() << " bytes" << endl;
+			cout << "The compressed text will take " << size_.size()/8 << " bytes" << endl;
+		
+		}
+
+		void print_codes(BT* top) {
+
+			if(top->left == NULL && top->right == NULL) {
+				cout << top->ch << " : " << "count: " <<  top->count << ", code: " << top->code << endl;
+				return;
+			}
+			
+			print_codes(top->right);
+			print_codes(top->left);
+
+		}
 	};
 
 #endif
